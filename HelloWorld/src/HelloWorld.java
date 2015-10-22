c143b48662ab4181dc119420279135a7546d7945
Martin Gernegroß <martin.gernegross@tum.de>
2015-10-22 08:46:11 +0200

    blablabla

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -11,8 +13,19 @@ public class HelloWorld {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		
 		System.out.println("Hello ASE2015 - how are you? :)");
+		
+		System.out.println("Please enter your name: ");
+		String username = in.nextLine();
+		in.close();
+		
+		HelloUser user = new HelloUser(username);
+		
+		user.greetUser();
 	}
 
 }
